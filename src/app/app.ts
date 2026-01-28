bc7c17d430f98ffc8edf1240e2f28c8793cf0d58
LucaPrevi0o <luca01.previati@edu.unife.it>
2026-01-28 11:20:25 +0100

    general refactoring

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  imports: [RouterLink],
-  styleUrl: './app.scss'
+  imports: [RouterLink, RouterOutlet],
+  styleUrls: ['./app.scss']
 })
 export class App {
   protected readonly title = signal('shop-frontend');
