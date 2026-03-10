e62c7ea9318bdbb50f8236f3277e7ba7ef938591
LucaPrevi0o <luca01.previati@edu.unife.it>
2026-03-10 09:58:17 +0100

    updated AuthInterceptor as const method

--- src/app/interceptors/auth-interceptor.ts
+++ src/app/interceptors/auth-interceptor.ts
@@ -1,15 +1,12 @@
-import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { inject } from '@angular/core';
+import { HttpInterceptorFn } from '@angular/common/http';
+import { UserService } from '../services/user-service';
 
-@Injectable()
-export class AuthInterceptor implements HttpInterceptor {
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const token = localStorage.getItem('jwtToken');
-    if (token) {
-      const authReq = req.clone({ setHeaders: { Authorization: `Bearer ${token}` } });
-      return next.handle(authReq);
-    }
-    return next.handle(req);
-  }
-}
+export const AuthInterceptor: HttpInterceptorFn = (req, next) => {
+
+  const auth = inject(UserService);
+  const token = auth.getToken();
+  if (!token) return next(req);
+  const authReq = req.clone({ setHeaders: { Authorization: `Bearer ${token}` } });
+  return next(authReq);
+};
