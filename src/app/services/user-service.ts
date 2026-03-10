9fb6ca34ba8495ddde3dc6025092293a91a0fd54
LucaPrevi0o <luca01.previati@edu.unife.it>
2026-03-10 09:57:06 +0100

    added userId to local storage to get logged user info

--- src/app/services/user-service.ts
+++ src/app/services/user-service.ts
@@ -1,9 +1,9 @@
 import { HttpClient } from "@angular/common/http";
 import { inject, Injectable } from "@angular/core";
-import { Router } from "@angular/router";
 import { API_BASE_URL } from "../app.config";
 import { User } from "../models/user";
 import { Observable } from "rxjs";
+import { tap } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
@@ -11,10 +11,30 @@ export class UserService {
     http = inject(HttpClient);
 
     login(user: User) : Observable<User> {
-        return this.http.post<User>(API_BASE_URL +'/login', { email: user.email, password: user.password });
+
+        return this.http.post<User>(API_BASE_URL +'/login', { email: user.email, password: user.password })
+            .pipe(
+                tap(res => {
+                    if (res?.token) localStorage.setItem('jwtToken', res.token);
+                    if (res?.id) localStorage.setItem('userId', String(res.id));
+                })
+            );
     }
+
+    getToken() : string | null { return localStorage.getItem('jwtToken'); }
+    getUserId() : string | null { return localStorage.getItem('userId'); }    
 
     isLoggedIn() : boolean { return !!localStorage.getItem('jwtToken'); }
 
-    logout() : void { localStorage.removeItem('jwtToken'); }
+    logout() : void {
+        
+        localStorage.removeItem('jwtToken');
+        localStorage.removeItem('userId');
+    }
+
+    getUser() : Observable<User> {
+        
+        const userId = localStorage.getItem('userId');
+        return this.http.get<User>(API_BASE_URL + '/users/' + userId);
+    }
 }
