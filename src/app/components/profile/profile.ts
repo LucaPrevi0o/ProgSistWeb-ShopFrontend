21ccfe1718ca3013a6a13e9a0365f66281ab42d3
LucaPrevi0o <luca01.previati@edu.unife.it>
2026-04-18 15:05:02 +0200

    updated profile page layout

--- src/app/components/profile/profile.ts
+++ src/app/components/profile/profile.ts
@@ -1,16 +1,17 @@
 import { Component, inject, OnInit } from "@angular/core";
-import { Observable } from "rxjs";
+import { Observable, take, switchMap } from "rxjs";
 import { HttpState, toHttpState} from "../../app.config";
 import { Router } from "@angular/router";
-import { AsyncPipe } from "@angular/common";
+import { AsyncPipe, JsonPipe } from "@angular/common";
 import { User } from "../../models/user";
 import { UserService } from "../../services/user-service";
 import { LoginRedirectorComponent } from "../login-redirector/login-redirector";
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-profile',
     standalone: true,
-    imports: [AsyncPipe, LoginRedirectorComponent],
+    imports: [AsyncPipe, JsonPipe, LoginRedirectorComponent, ReactiveFormsModule],
     templateUrl: './profile.html',
     styleUrls: ['./profile.scss']
 })
@@ -19,7 +20,16 @@ export class ProfileComponent implements OnInit {
     state$!: Observable<HttpState<User>>;
     router = inject(Router);
 
-    constructor(private userService: UserService) {}
+    profileForm!: FormGroup;
+
+    constructor(private userService: UserService, private fb: FormBuilder) {
+
+        this.profileForm = this.fb.group({
+            firstName: [''],
+            lastName: [''],
+            phone: ['']
+        });
+    }
 
     ngOnInit() : void { this.state$ = toHttpState(this.userService.getUser()); }
 
@@ -29,5 +39,26 @@ export class ProfileComponent implements OnInit {
         
         this.userService.logout();
         this.router.navigate(['/login']);
+    }
+
+    saveInfo() : void {
+
+        if (this.profileForm.invalid) return;
+
+        const v = this.profileForm.value;
+
+        // Decide create vs update by fetching current user once, then call the appropriate endpoint
+        const op$ = this.userService.getUser().pipe(
+            take(1),
+            switchMap(user => {
+                if (user?.info) {
+                    return this.userService.updateUserInfo({ firstName: v.firstName, lastName: v.lastName, phone: v.phone });
+                }
+                return this.userService.createUserInfo({ firstName: v.firstName, lastName: v.lastName, phone: v.phone });
+            }),
+            switchMap(() => this.userService.getUser())
+        );
+
+        this.state$ = toHttpState(op$);
     }
 }
