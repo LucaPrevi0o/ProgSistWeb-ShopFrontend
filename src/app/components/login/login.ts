ddd502ba24ead987a6e0390dcb45af2deea6e62c
Luca Previati <luca@previati.com>
2026-06-10 08:49:53 +0200

    Redirect admins to dashboard after login

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -7,44 +7,4 @@ import { HttpState, toHttpState } from "../../app.config";
 import { AsyncPipe } from "@angular/common";
 import { Router } from "@angular/router";
 
-@Component({
-    selector: 'app-login',
-    templateUrl: './login.html',
-    imports: [AsyncPipe, ReactiveFormsModule],
-    styleUrls: ['./login.scss']
-})
-export class LoginComponent implements OnInit {
-
-    loginForm: FormGroup;
-    state$! : Observable<HttpState<User>>;
-    router = inject(Router);
-
-    constructor(private fb: FormBuilder, private userService: UserService) {
-
-        this.loginForm = this.fb.group({
-            email: ['', [Validators.required, Validators.email]],
-            password: ['', Validators.required]
-        });
-    }
-
-    ngOnInit() : void { 
-        
-        if (this.userService.isLoggedIn()) this.userService.logout();
-        this.reset();
-    }
-
-    submit() : void {
-
-        if (this.loginForm.invalid) return;
-        var login = this.userService.login(this.loginForm.value as User);
-        this.state$ = toHttpState(login).pipe(
-            tap(state => { if (state.status === 'success') {
-                
-                localStorage.setItem('jwtToken', state.data.token);
-                this.router.navigate(['/products']);
-            }})
-        );
-    }
-
-    reset() : void { this.state$ = of({ status: 'empty' } as HttpState<User>); }
-}
+@Component
